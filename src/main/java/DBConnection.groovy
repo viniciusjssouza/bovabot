cc0c0d957b8da685e967a8151dc88ed1ff36d48b
viniciusjssouza <viniciusjssouza@gmail.com>
2018-10-13 15:58:59 -0300

    migrate to mysql 8

--- src/main/java/DBConnection.groovy
+++ src/main/java/DBConnection.groovy
@@ -13,7 +13,7 @@ class DBConnection {
     def static final CONNECTION_URL = "jdbc:mysql://localhost:3306/bovespa"
     def static final USER_NAME = "bovabot"
     def static final PASSWORD = "griever"
-    def static final DRIVER_NAME = "com.mysql.jdbc.Driver"
+    def static final DRIVER_NAME = "com.mysql.cj.jdbc.Driver"
 
     def private Sql conn;
 
